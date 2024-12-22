bd3e404a76d9822671be19f49952ef43fab4492e
oscar marina <oscar7600@outlook.com>
2024-12-22 22:09:07 +0100

    chore: initial commit

---
+++ src/styles/codemirror-component-styles.css.ts
@@ -0,0 +1,34 @@
+import {css} from 'lit';
+
+export const styles = css`
+  :host {
+    display: block;
+    max-width: var(--codemirror-maxwidth, 50%);
+    width: var(--codemirror-width, 100%);
+    height: var(--codemirror-height, 100%);
+    box-sizing: border-box;
+  }
+
+  :host([hidden]),
+  [hidden] {
+    display: none !important;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  .wrapper {
+    width: inherit;
+    height: inherit;
+  }
+
+  .cm-editor {
+    height: inherit;
+    font: inherit;
+    direction: ltr;
+    -webkit-tap-highlight-color: transparent;
+  }
+`;
---
+++ src/styles/monaco-editor-component-styles.css.ts
@@ -0,0 +1,27 @@
+import {css} from 'lit';
+
+export const styles = css`
+  :host {
+    display: block;
+    max-width: var(--moncaco-editor-maxwidth, 50%);
+    width: var(--moncaco-editor-width, 100%);
+    height: var(--moncaco-editor-height, 100%);
+    box-sizing: border-box;
+  }
+
+  :host([hidden]),
+  [hidden] {
+    display: none !important;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  .wrapper {
+    width: inherit;
+    height: inherit;
+  }
+`;
